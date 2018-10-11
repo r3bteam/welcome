9af28cbd721f4ca472c51d25ee32434b56b8e384
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-10-11 17:30:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,44 +95,54 @@ const w = ['./welcome.png'];
 })
 });
 
+onst devs = ["450711003324350485"]
+
+const adminprefix = "+";
 client.on('message', message => {
-var prefix = "+";
- 
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id == '450711003324350485' ) return;
- 
-if (message.content.startsWith(prefix + 'playing')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult}** : تم تغيير الحالة`)
-} else
- 
-if (message.content.startsWith(prefix + 'stream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/Elegant");
-    message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
-} else
- 
-if (message.content.startsWith(prefix + 'watch')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith(prefix + 'listg')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(prefix + 'name')) {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+    if (message.content === (adminprefix + "Percie")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else     
+    if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
-      message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
-  return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
-} else
-
-if (message.content.startsWith(prefix + 'pic')) {
+      message.channel.sendMessage(`**${argresult}** : Done :>`)
+  return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+  } else
+    if (message.content.startsWith(adminprefix + 'setavatar')) {
   client.user.setAvatar(argresult);
     message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-}
-});
+        } else     
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  }
+    if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
+        console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log(`⚠️ Bot restarting... ⚠️`);
+        console.log("===============================================\n\n");
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(`Bot Successfully Restarted`);
+    }
+  
+  });
+
 
 
 client.on("guildMemberAdd", (member) => {
@@ -180,5 +190,68 @@ client.guilds.get(member.guild.id).channels.get(channel).send(`**Invited By** ${
        });
     });
 });
+
+const Discord = require("discord.js");
+const RichEmbed = require("discord.js");
+const { Client, Util } = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('message',async message => {
+    var prefix = "+";
+    if(message.content.startsWith(prefix + "bc")) {
+      if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send(':regional_indicator_b::regional_indicator_c:| **ارسل الرسالة الان**').then(msg => {
+  
+      let awaitM = message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      })
+      .then(collected => {
+        collected.first().delete();
+        thisMessage = collected.first().content;
+        msg.edit(':regional_indicator_b::regional_indicator_c:| **هل انت متأكد؟**');
+        let awaitY = message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 20000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+          message.guild.members.forEach(member => {
+            msg.edit(':regional_indicator_b::regional_indicator_c:| **جاري الارسال**');
+            collected.first().delete();
+            member.send(`${thisMessage}\n\n${member}`);
+          });
+          }
+        });
+      });
+      });
+    }
+  });
+    
+                    client.on("message", message => {
+                         var prefix = "+";
+                        if (message.content.startsWith(prefix + "obc")) {
+                                     if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+              let args = message.content.split(" ").slice(1);
+              var argresult = args.join(' '); 
+              message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+             m.send(`${argresult}\n ${m}`);
+            })
+             message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+             message.delete(); 
+            };     
+            });
 
 client.login(process.env.BOT_TOKEN);
