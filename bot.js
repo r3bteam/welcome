8f4c7a3402a953e6cbed12666fcc2d330af77fde
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-10-15 18:34:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -249,5 +249,22 @@ client.on('message',async message => {
              message.delete(); 
             };     
             });
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
 
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 5,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**مدة الرابط : يـوم
+عدد استخدامات الرابط : 5**`)
+
+
+    }
+});
 client.login(process.env.BOT_TOKEN);
